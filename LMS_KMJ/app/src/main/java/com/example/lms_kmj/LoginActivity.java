9be1b38aa84f570ad31bf4aff8340c0703453eaf
MINJU KIM <yd5726@naver.com>
2023-01-03 14:34:55 +0900

    last LMS_KMJ
    last LMS_KMJ

---
+++ LMS_KMJ/app/src/main/java/com/example/lms_kmj/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.lms_kmj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    Button login_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login_btn = findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
