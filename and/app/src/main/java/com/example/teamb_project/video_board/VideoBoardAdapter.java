fd704d7dfc99dadf01a7b370e7e499835185757e
wonhee <asdkcone13@gmail.com>
2023-01-11 19:07:31 +0900

    강의영상 제목 검색

--- and/app/src/main/java/com/example/teamb_project/video_board/VideoBoardAdapter.java
+++ and/app/src/main/java/com/example/teamb_project/video_board/VideoBoardAdapter.java
@@ -1,23 +1,29 @@
 package com.example.teamb_project.video_board;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.teamb_project.R;
+import com.example.teamb_project.vo.BoardVO;
 
 import java.util.ArrayList;
 
 public class VideoBoardAdapter extends RecyclerView.Adapter<VideoBoardAdapter.ViewHolder>{
     LayoutInflater inflater;
-    ArrayList<Object> list;
+    ArrayList<BoardVO> list;
+    Context context;
 
-    public VideoBoardAdapter(LayoutInflater inflater, ArrayList<Object> list) {
+    public VideoBoardAdapter(LayoutInflater inflater, ArrayList<BoardVO> list, Context context) {
         this.inflater = inflater;
         this.list = list;
+        this.context = context;
     }
 
     @NonNull
@@ -27,13 +33,16 @@ public class VideoBoardAdapter extends RecyclerView.Adapter<VideoBoardAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder h, int i) {
+        //특정 강의영상 게시글 클릭
+        h.board.setOnClickListener(v -> {
+            Intent intent = new Intent();
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 10;
+        return 12;
     }
     @Override
     public long getItemId(int i){return i;}
@@ -41,9 +50,10 @@ public class VideoBoardAdapter extends RecyclerView.Adapter<VideoBoardAdapter.Vi
     public int getItemViewType(int i){return i;}
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        LinearLayout board;
         public ViewHolder(@NonNull View v) {
             super(v);
+            board = v.findViewById(R.id.lin_board);
         }
     }
 }
