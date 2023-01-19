5df8a4f6d18fef15b959b20a4e6e8433e6482786
wonhee <asdkcone13@gmail.com>
2023-01-19 16:05:53 +0900

    temp

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/teamb_project/BR.java
@@ -0,0 +1,5 @@
+package com.example.teamb_project;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ app/build/generated/data_binding_trigger/debug/com/example/teamb_project/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.teamb_project;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
